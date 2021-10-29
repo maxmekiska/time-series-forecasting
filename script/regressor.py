73b34c71c8bf82f87de88839d1dddedfcf2af6e6
maxmekiska <maxmekiska@gmail.com>
2021-10-30 00:57:49 +0100

    added scaling option

--- script/regressor.py
+++ script/regressor.py
@@ -1,11 +1,12 @@
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import StandardScaler
 
 
 class Regressor:
     
-    def __init__(self, time_series, look_back, look_future) -> object:
+    def __init__(self, time_series, look_back, look_future, scale=False) -> object:
         '''
             Parameters:
                 steps_past (int): Steps predictor will look backward.
@@ -13,6 +14,14 @@ class Regressor:
                 data (DataFrame): Input data for model training.
         '''
         self.X, self.y = self._sliding_window(time_series, look_back, look_future)
+
+        self.scaler = self._normalize_data(self.X)
+
+        self.scale = scale
+        
+        if self.scale == True:
+            self.X = self.scaler.transform(self.X)
+
     
         
     def _sliding_window(self, _list, look_back, look_front):
@@ -38,8 +47,19 @@ class Regressor:
     
     def get_sequences(self):
         return self.X, self.y
-    
+
+    def _normalize_data(self, data):
+        scaler = StandardScaler().fit(data)
+        #scaled_data = scaler.transform(data)
+        return scaler #scaled_data
+
+
     def forecast(self, data):
+        if self.scale == True:
+            data = self.scaler.transform([data])
+        else:
+            data = [data]
+
         model_1 = KNeighborsRegressor()
         model_2 = DecisionTreeRegressor()
         model_3 = RandomForestRegressor()
@@ -47,8 +67,8 @@ class Regressor:
         model_2.fit(self.X, self.y)
         model_3.fit(self.X, self.y)
         
-        yhat_1 = model_1.predict([data])
-        yhat_2 = model_2.predict([data])
-        yhat_3 = model_3.predict([data])
+        yhat_1 = model_1.predict(data)
+        yhat_2 = model_2.predict(data)
+        yhat_3 = model_3.predict(data)
         
         return yhat_1, yhat_2, yhat_3
