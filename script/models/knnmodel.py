560ba25ff2a21e9e767fd7e8bd95f40786c448ad
maxmekiska <maxmekiska@gmail.com>
2021-11-14 02:41:51 +0000

    knn grid parameter adjustments

--- script/models/knnmodel.py
+++ script/models/knnmodel.py
@@ -5,7 +5,6 @@ KNNHYPARAM = {
 "n_neighbors": [3, 5, 7, 8, 10, 12],
 "weights":['uniform', 'distance'],
 "algorithm": ['ball_tree', 'kd_tree', 'brute', 'auto'],
-"leaf_size": [30, 40, 50],
-"p":[1, 2, 3, 4],
-"metric":['minkowski']
+"leaf_size": [ 15, 30, 40, 50, 100],
+"p":[1, 2, 3, 4]
 }
