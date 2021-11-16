ecd8cc1c45a29162e2d0e3fb2b202edba676a90d
maxmekiska <maxmekiska@gmail.com>
2021-11-16 01:16:48 +0000

    fixing needed for random forest gird search parameters

--- script/models/randomforest.py
+++ script/models/randomforest.py
@@ -1,12 +1,12 @@
 from sklearn.ensemble import RandomForestRegressor
 
 RFORESTHYPARAM = {
-"n_estimatorsint": [100, 130, 150, 170],
-"criterion":[“gini”, “entropy”],
-"min_samples_splitint": [2, 3, 4, 5],
-"min_samples_leafint": [1, 2, 3],
-"max_features": [“auto”, “sqrt”, “log2”],
-"bootstrapbool": [True, False],
-"oob_scorebool": [False, True],
-"class_weight": [“balanced”, “balanced_subsample”]
+#"n_estimatorsint": [100, 130, 150, 170],
+"criterion":['gini', 'entropy'],
+#"min_samples_splitint": [2, 3, 4, 5],
+#"min_samples_leafint": [1, 2, 3],
+#"max_features": ['auto', 'sqrt', 'log2'],
+#"bootstrapbool": [True, False],
+#"oob_scorebool": [False, True],
+#"class_weight": ['balanced', 'balanced_subsample']
 }
