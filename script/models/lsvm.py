a437a81b8cbbbc71559056ffcb84948e5d438623
maxmekiska <maxmekiska@gmail.com>
2021-11-17 21:42:10 +0000

    added hyperparameter to grid

--- script/models/lsvm.py
+++ script/models/lsvm.py
@@ -1,1 +1,8 @@
 from sklearn.svm import LinearSVR
+
+LSVRYPARAM = {
+"loss": ['epsilon_insensitive', 'squared_epsilon_insensitive'],
+"epsilon":[0.0, 0.1, 0.3, 0.6],
+"C": [1.0, 1.5, 2, 3],
+"tol":[1e-4, 1e-5]
+}
