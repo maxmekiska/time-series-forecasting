249081cf22370c4df0a35802cda6b3d10ab506aa
maxmekiska <maxmekiska@gmail.com>
2021-11-18 23:51:29 +0000

    new versions of grid search

--- script/utils/gridsearch.py
+++ script/utils/gridsearch.py
@@ -1,11 +1,28 @@
 from sklearn.experimental import enable_halving_search_cv
 from sklearn.model_selection import HalvingGridSearchCV
 from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+
 
 
 def grids(model: object, parameters: dict, X_train: list, y_train: list):
     grid = GridSearchCV(model, parameters, verbose=1)
     grid.fit(X_train, y_train)
     optimal_parameters = grid.best_params_
-    print('Best score:', grid.best_score_)
+    print('Grid Search ' + model)
+    print('Best score R2:', grid.best_score_)
     return optimal_parameters
+
+def grids_halv(model: object, parameters: dict, X_train: list, y_train: list):
+    grid = HalvingGridSearchCV(model, parameters, verbose=1)
+    grid.fit(X_train, y_train)
+    optimal_parameters = grid.best_params_
+    print('Halving Grid Search ' + model)
+    print('Best score R2:', grid.best_score_)
+
+def grids_random(model: object, parameters: dict, X_train: list, y_train: list):
+    grid = RandomizedSearchCV(model, parameters, verbose=1)
+    grid.fit(X_train, y_train)
+    optimal_parameters = grid.best_params_
+    print('Randomized Grid Search ' + model)
+    print('Best score R2:', grid.best_score_)
