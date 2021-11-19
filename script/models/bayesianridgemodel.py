1b95890ffd36f7c41e53dac727a76ec8bc8e3a71
maxmekiska <maxmekiska@gmail.com>
2021-11-19 21:52:46 +0000

    optimization hyperparameters set for bayesian ridge regressor

---
+++ script/models/bayesianridgemodel.py
@@ -0,0 +1,10 @@
+from sklearn.linear_model import BayesianRidge
+
+BAYRIDGEHYPARAM = {
+"pipe__estimator__n_iter": [300, 350, 400, 450],
+"pipe__estimator__tol":[1e-3, 1e-4],
+"pipe__estimator__alpha_1": [1e-6, 1e-5, 1e-7]
+"pipe__estimator__alpha_2": [1e-6, 1e-5, 1e-7]
+"pipe__estimator__lambda_1": [1e-6, 1e-5, 1e-7]
+"pipe__estimator__lambda_2": [1e-6, 1e-5, 1e-7]
+}
